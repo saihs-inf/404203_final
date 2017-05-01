6299d63f980f0ffa738dc052cd2c734295adc7bd
youhua <a0915881202@gmail.com>
2017-05-01 12:05:00 +0800

    button1

--- 404203_final/Form1.cs
+++ 404203_final/Form1.cs
@@ -21,5 +21,10 @@ namespace _404203_final
         {
             MessageBox.Show("hello world");
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("button1");
+        }
     }
 }
